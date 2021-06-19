91df057708ac80974e37dadf53e73c2c6591cfe9
thisum <thisum.kankanamge@moht.com.sg>
2021-06-19 20:05:53 +0800

    first commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+	redisHost: process.env.REDIS_HOST,
+	redisPort: process.env.REDIS_PORT,
+	pgUser: process.env.PGUSER,
+	pgHost: process.env.PGHOST,
+	pgDB: process.env.PGDB,
+	pgPW: process.env.PGPW,
+	pgPort: process.env.PGPORT
+};
